b1eef7c955b7e200b0115b483a1ae276e892de04
Khant Min Kyi <khantminkyi@gmail.com>
2024-01-02 23:16:59 +0630

    Change Theme Mode Toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports =  {
-  darkMode: 'class',
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    colors: {
-      mainbody: {
-        100: '#cffafe',
-        200: '#a5f3fc',
-        300: '#6BABA3',
-        400: '#22d3ee',
-        500: '#06b6d4',
-        600: '#426B66',
-        700: '#3B5D5A',
-        800: '#223735',
-        900: '#152221',
-      },
+module.exports = {
+    darkMode: "class",
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        colors: {
+            mainbody: {
+                100: "#cffafe",
+                200: "#a5f3fc",
+                300: "#6BABA3",
+                400: "#22d3ee",
+                500: "#06b6d4",
+                600: "#426B66",
+                700: "#3B5D5A",
+                800: "#223735",
+                900: "#152221",
+            },
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  plugins: [
-      require('flowbite/plugin')
-  ],
-}
-
+    plugins: [require("flowbite/plugin")],
+};
