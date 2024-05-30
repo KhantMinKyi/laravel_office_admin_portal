20b24d492eb4697e3630d8c6727c3fda78c06e43
Khant Min Kyi <khantminkyi@gmail.com>
2024-05-30 09:33:14 +0630

    Update key_management_list.blade.php

--- resources/views/admins/key_management/key_management_list.blade.php
+++ resources/views/admins/key_management/key_management_list.blade.php
@@ -229,8 +229,8 @@
                                                     class="fa-regular fa-eye text-mainbody-100 hover:text-white  dark:text-mainbody-400 mr-1"></i>
                                             </a>
                                             {{-- view dot --}}
-                                            <button id="apple-imac-{{ $key->id }}-dropdown-button"
-                                                data-dropdown-toggle="apple-imac-{{ $key->id }}-dropdown"
+                                            <button id="apple-imac-{{ $key->id }}111-dropdown-button"
+                                                data-dropdown-toggle="apple-imac-{{ $key->id }}111-dropdown"
                                                 class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                                                 type="button">
                                                 <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
@@ -239,7 +239,7 @@
                                                         d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                 </svg>
                                             </button>
-                                            <div id="apple-imac-{{ $key->id }}-dropdown"
+                                            <div id="apple-imac-{{ $key->id }}111-dropdown"
                                                 class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-mainbody-700 dark:divide-mainbody-600">
                                                 <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
                                                     aria-labelledby="apple-imac-27-dropdown-button">
