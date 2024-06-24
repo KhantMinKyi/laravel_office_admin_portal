a69b1cc34d1a74534d58ea9f5f3018c3431c0956
Khant Min Kyi <khantminkyi@gmail.com>
2024-06-24 15:42:07 +0630

    location management branch cipher added

--- resources/views/admins/partial_view/location_management/branch/branch_list.blade.php
+++ resources/views/admins/partial_view/location_management/branch/branch_list.blade.php
@@ -1,23 +1,97 @@
+<?php
+use App\Models\KeyPermission;
+$encryption_keys = KeyPermission::where('user_id', Auth::user()->id)
+    ->where('is_granded', 1)
+    ->where('is_active', 1)
+    ->get();
+?>
 <div class="bg-white dark:bg-mainbody-800 relative shadow-md sm:rounded-lg overflow-hidden">
     {{-- Search Bar --}}
     <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-        <div class="w-full md:w-1/2">
-            <form class="flex items-center">
-                <label for="simple-search" class="sr-only">Search</label>
-                {{-- <div class="relative w-full">
-                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                            viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                    <input type="text" id="simple-search"
-                        class="search bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainbody-500 focus:border-mainbody-500 block w-full pl-10 p-2 dark:bg-mainbody-700 dark:border-mainbody-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-mainbody-600 dark:focus:border-mainbody-600"
-                        placeholder="Search" required="">
-                </div> --}}
-            </form>
+
+        {{-- For City --}}
+        <div>
+            <select id="view_city_for_branch_encryption_key" name="view_city_for_branch_encryption_key"
+                class="encryption_key bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainbody-300 focus:border-mainbody-300 block w-full p-2.5 dark:bg-mainbody-700 dark:border-mainbody-600 dark:placeholder-mainbody-300 dark:text-white dark:focus:ring-mainbody-800 dark:focus:border-mainbody-800">
+                <option selected disabled>Select City Encryption Key</option>
+                @foreach ($encryption_keys as $encryption_key)
+                    <option value="{{ $encryption_key->key }}"
+                        data-view_city_for_branch_encryption_key="{{ $encryption_key->key }}">
+                        {{ $encryption_key->key_description->name }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+        <div class="flex items-center space-x-3 w-full md:w-auto">
+            <button id="actionsDropdownButtonBranchCity" data-dropdown-toggle="actionsDropdownBranchCity"
+                class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-mainbody-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-mainbody-800 dark:text-gray-400 dark:border-mainbody-600 dark:hover:text-white dark:hover:bg-mainbody-700"
+                type="button">
+                <svg class="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path clip-rule="evenodd" fill-rule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+                </svg>
+                Actions
+            </button>
+            <div id="actionsDropdownBranchCity"
+                class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-mainbody-700 dark:divide-mainbody-600">
+                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="actionsDropdownButtonBranchCity">
+                    <li>
+                        <a href="#" id="btnEncryptViewCityForBranch"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-mainbody-600 dark:hover:text-white"><i
+                                class="fa-solid fa-key mr-2"></i> <i> Encrypt All</i> </a>
+                    </li>
+                </ul>
+                <div class="py-1">
+                    <a href="#" id="btnDecryptViewCityForBranch"
+                        class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-mainbody-600 dark:text-gray-200 dark:hover:text-white">
+                        <i class="fa-solid fa-lock-open mr-2"></i>
+                        <i>Decrypt All</i></a>
+                </div>
+            </div>
+        </div>
+        {{-- For Township --}}
+        <div>
+            <select id="view_township_for_branch_encryption_key" name="view_township_for_branch_encryption_key"
+                class="encryption_key bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainbody-300 focus:border-mainbody-300 block w-full p-2.5 dark:bg-mainbody-700 dark:border-mainbody-600 dark:placeholder-mainbody-300 dark:text-white dark:focus:ring-mainbody-800 dark:focus:border-mainbody-800">
+                <option selected disabled>Select Township Encryption Key</option>
+                @foreach ($encryption_keys as $encryption_key)
+                    <option value="{{ $encryption_key->key }}"
+                        data-view_township_for_branch_encryption_key="{{ $encryption_key->key }}">
+                        {{ $encryption_key->key_description->name }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+        <div class="flex items-center space-x-3 w-full md:w-auto">
+            <button id="actionsDropdownButtonTownshipForBranch" data-dropdown-toggle="actionsDropdownTownshipForBranch"
+                class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-mainbody-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-mainbody-800 dark:text-gray-400 dark:border-mainbody-600 dark:hover:text-white dark:hover:bg-mainbody-700"
+                type="button">
+                <svg class="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path clip-rule="evenodd" fill-rule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+                </svg>
+                Actions
+            </button>
+            <div id="actionsDropdownTownshipForBranch"
+                class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-mainbody-700 dark:divide-mainbody-600">
+                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="actionsDropdownButtonTownshipForBranch">
+                    <li>
+                        <a href="#" id="btnEncryptViewTownshipForBranch"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-mainbody-600 dark:hover:text-white"><i
+                                class="fa-solid fa-key mr-2"></i> <i> Encrypt All</i> </a>
+                    </li>
+                </ul>
+                <div class="py-1">
+                    <a href="#" id="btnDecryptViewTownshipForBranch"
+                        class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-mainbody-600 dark:text-gray-200 dark:hover:text-white">
+                        <i class="fa-solid fa-lock-open mr-2"></i>
+                        <i>Decrypt All</i></a>
+                </div>
+            </div>
         </div>
         <div
             class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
@@ -57,8 +131,9 @@
                             class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ $branch_key + 1 }}</th>
                         <td class="px-4 py-3">{{ $branch->name }}</td>
-                        <td class="px-4 py-3">{{ $branch->city->name }}</td>
-                        <td class="px-4 py-3">{{ $branch->township->name }}</td>
+                        <td class="px-4 py-3 data_input_view_city_for_branch">{{ $branch->city->name }}</td>
+                        <td class="px-4 py-3 data_input_view_township_for_branch">{{ $branch->township->name }}
+                        </td>
                         <td class="px-4 py-3">
                             <Address>{{ $branch->address }}</Address>
                         </td>
@@ -108,3 +183,9 @@
     {{-- Pagination --}}
 
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+    integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="/js/admin/location_management/township/view_encryption_for_branch.js"></script>
+<script src="/js/admin/location_management/city/view_encryption_for_branch.js"></script>
--- resources/views/admins/partial_view/location_management/branch/create_branch.blade.php
+++ resources/views/admins/partial_view/location_management/branch/create_branch.blade.php
@@ -1,7 +1,14 @@
+<?php
+use App\Models\KeyPermission;
+$encryption_keys = KeyPermission::where('user_id', Auth::user()->id)
+    ->where('is_granded', 1)
+    ->where('is_active', 1)
+    ->get();
+?>
 <div class="fixed top-10 right-0 bottom-0 left-0 bg-black opacity-50">
 </div>
 <div class="flex items-center justify-center">
-    <div class="p-10 bg-white text-black dark:bg-mainbody-800 dark:text-white relative rounded w-1/2 xl:w-1/3">
+    <div class="p-10 bg-white text-black dark:bg-mainbody-800 dark:text-white relative rounded ">
         <button class="absolute top-0 right-0 px-4 pt-4 " onclick="togglePopupBranchAddModel()">
             <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
                 <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
@@ -22,12 +29,44 @@
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainbody-300 focus:border-mainbody-300 block w-full p-2.5 dark:bg-mainbody-700 dark:border-mainbody-600 dark:placeholder-mainbody-300 dark:text-white dark:focus:ring-mainbody-800 dark:focus:border-mainbody-800">
                             <option selected disabled>Select City</option>
                             @foreach ($cities as $city)
-                                <option value="{{ $city->id }}">{{ $city->name }}</option>
+                                <option value="{{ $city->id }}" class="data_input_create_city_for_branch">
+                                    {{ $city->name }}</option>
                             @endforeach
                         </select>
                     </div>
                 </div>
-                <div class="grid gap-6 mb-6 md:grid-cols-1">
+                {{-- For City --}}
+                <div class="encrypt-model " id="encrypt-model">
+                    <div class="grid gap-6 mt-6 md:grid-cols-2">
+                        <div>
+                            <label for="create_city_for_branch_encryption_key"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Encryption
+                                Key For City </label>
+                            <select id="create_city_for_branch_encryption_key"
+                                name="create_city_for_branch_encryption_key"
+                                class="encryption_key bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainbody-300 focus:border-mainbody-300 block w-full p-2.5 dark:bg-mainbody-700 dark:border-mainbody-600 dark:placeholder-mainbody-300 dark:text-white dark:focus:ring-mainbody-800 dark:focus:border-mainbody-800">
+                                <option selected disabled>Select Encryption Key</option>
+                                @foreach ($encryption_keys as $encryption_key)
+                                    <option value="{{ $encryption_key->key }}"
+                                        data-create_city_for_branch_encryption_key="{{ $encryption_key->key }}">
+                                        {{ $encryption_key->key_description->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            <a href="#" id="btnEncryptCreateCityForBranch"
+                                class=" float-right mt-6 text-white bg-mainbody-300 hover:bg-mainbody-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-mainbody-600 dark:hover:bg-mainbody-700 dark:focus:ring-mainbody-800">Encrypt</a>
+                            <a href="#" id="btnDecryptCreateCityForBranch"
+                                class=" float-right mt-6 mr-4 text-white bg-mainbody-300 hover:bg-mainbody-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-mainbody-600 dark:hover:bg-mainbody-700 dark:focus:ring-mainbody-800">Decrypt</a>
+                        </div>
+                    </div>
+                    {{-- <div>
+                        <button type="submit"
+                            class="content-end mt-2 text-white bg-mainbody-300 hover:bg-mainbody-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-mainbody-600 dark:hover:bg-mainbody-700 dark:focus:ring-mainbody-800">Generate</button>
+                    </div> --}}
+                </div>
+                <div class="grid gap-6 my-6 md:grid-cols-1">
                     <div>
                         <label for="township_id"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Township</label>
@@ -35,10 +74,42 @@
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainbody-300 focus:border-mainbody-300 block w-full p-2.5 dark:bg-mainbody-700 dark:border-mainbody-600 dark:placeholder-mainbody-300 dark:text-white dark:focus:ring-mainbody-800 dark:focus:border-mainbody-800">
                             <option selected disabled>Select Township</option>
                             @foreach ($townships as $township)
-                                <option value="{{ $township->id }}">{{ $township->name }}</option>
+                                <option value="{{ $township->id }}" class="data_input_create_township_for_branch">
+                                    {{ $township->name }}</option>
                             @endforeach
                         </select>
                     </div>
+                </div>
+                {{-- For Township --}}
+                <div class="encrypt-model " id="encrypt-model">
+                    <div class="grid gap-6 mt-6 md:grid-cols-2">
+                        <div>
+                            <label for="create_township_for_branch_encryption_key"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Encryption
+                                Key For Township </label>
+                            <select id="create_township_for_branch_encryption_key"
+                                name="create_township_for_branch_encryption_key"
+                                class="encryption_key bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-mainbody-300 focus:border-mainbody-300 block w-full p-2.5 dark:bg-mainbody-700 dark:border-mainbody-600 dark:placeholder-mainbody-300 dark:text-white dark:focus:ring-mainbody-800 dark:focus:border-mainbody-800">
+                                <option selected disabled>Select Encryption Key</option>
+                                @foreach ($encryption_keys as $encryption_key)
+                                    <option value="{{ $encryption_key->key }}"
+                                        data-create_township_for_branch_encryption_key="{{ $encryption_key->key }}">
+                                        {{ $encryption_key->key_description->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            <a href="#" id="btnEncryptCreateTownshipForBranch"
+                                class=" float-right mt-6 text-white bg-mainbody-300 hover:bg-mainbody-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-mainbody-600 dark:hover:bg-mainbody-700 dark:focus:ring-mainbody-800">Encrypt</a>
+                            <a href="#" id="btnDecryptCreateTownshipForBranch"
+                                class=" float-right mt-6 mr-4 text-white bg-mainbody-300 hover:bg-mainbody-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-mainbody-600 dark:hover:bg-mainbody-700 dark:focus:ring-mainbody-800">Decrypt</a>
+                        </div>
+                    </div>
+                    {{-- <div>
+                        <button type="submit"
+                            class="content-end mt-2 text-white bg-mainbody-300 hover:bg-mainbody-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-mainbody-600 dark:hover:bg-mainbody-700 dark:focus:ring-mainbody-800">Generate</button>
+                    </div> --}}
                 </div>
                 <div class="grid gap-6 mb-6 md:grid-cols-1">
                     <div>
@@ -58,3 +129,8 @@
 
     </div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+    integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="/js/admin/location_management/township/create_encryption_for_branch.js"></script>
+<script src="/js/admin/location_management/city/create_encryption_for_branch.js"></script>
